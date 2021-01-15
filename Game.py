78cc377abc9451b7d7d08bcd748607ec7a7a723a
Trent S <tjschweitzer@gmail.com>
2021-01-15 16:14:55 -0700

    Inital game code works

--- Game.py
+++ Game.py
@@ -1,57 +1,109 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from scipy import signal
 
 
-M = np.zeros((16, 16))
+def game(A,bc = "Periodic"):
 
-def _init(row,col):
-    global M
-    M = np.zeros((row,col), dtype=int)
+    A_NN = nearest_neibour_interp_fast(A,bc)
 
-    M[1,1]=1
-    M[2,2]=1
-    M[3,3]=1
-    M[4,4]=1
-    M[5,5]=1
-    M[4,5]=1
-    M[2, 5] = 1
-    M[1, 5] = 1
+    remains_alive_condition = np.logical_and(A==1, np.logical_or(A_NN==2, A_NN==3))
+    random_life = np.logical_and(A==0, A_NN==3)
 
-    M[5, 4] = 1
-    M[5, 3] = 1
-    M[5,2]=1
-    M[5,1]=1
+    A_new = np.where(np.logical_or(remains_alive_condition,random_life),1,0)
+
+    return A_new
+
+def nearest_neibour_interp_fast(A, bc):
+
+    k = [[True, True, True],
+         [True, False, True],
+         [True, True, True]]
+
+    if bc == "Periodic":
+        convolution = signal.convolve2d(A, k, mode='same', boundary='wrap')
+    elif bc == "Symmetric":
+        convolution = signal.convolve2d(A, k, mode='same', boundary='symm')
+    else:
+        convolution = signal.convolve2d(A, k, mode='same', boundary='fill')
+    # print(convolution)
+    return convolution
 
 
-def check(N):
-    count = N.sum()
-    if N[1,1] == 0 and count == 2:
-        return 1
-    if N[1,1] == 1 and (count ==3 or count == 4):
-        return 1
-    return 0
+def _init(size, p=.5, shape="Random"):
+    if shape == "Random":
+        A = np.random.random(size)
+
+        return np.where(A > p, 0, 1)
+    A = np.zeros(size)
+    if shape == 1:
+        A[3, 3] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+    if shape == 2:
+        A[3, 3] = 1
+        A[4, 4] = 1
+        A[3, 4] = 1
+    if shape == 3:
+        A[3, 3] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+        A[6, 3] = 1
+    if shape == 4:
+        A[3, 3] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+        A[4, 4] = 1
+    if shape == 5:
+        A[3, 3] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+        A[4, 4] = 1
+    if shape == 6:
+        A[3, 3] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+        A[5, 2] = 1
+        A[4, 1] = 1
+    if shape == 7:
+        A[3, 4] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+        A[5, 2] = 1
+        A[4, 1] = 1
+    if shape == 8:
+        A[3, 3] = 1
+        A[4, 3] = 1
+        A[5, 3] = 1
+        A[4, 4] = 1
+        A[5, 4] = 1
+        A[6, 4] = 1
+
+    return A
+
+
+
+
+
+
+
+
+inital_prob = 0.7
+life = _init((32,32), inital_prob,5)
+
+frames = 1000
+
+fig, ax = plt.subplots(constrained_layout=False)
+image = ax.imshow(life)
 
 
 def update(i):
-    global M
-    row, col =  M.shape
-    temp = np.zeros((row,col))
-    for i in range(row):
-        for j in range(row):
-            neighborhood = np.zeros((3,3))
-            for k in range(-1,1):
-                for l in range(-1, 1):
-                    neighborhood[k+1,l+1] = M[(k+i+row)%row,(l+j+col)%col]
-            temp[i,j]= check(neighborhood)
-    M=temp
-    matrice.set_array(M)
+    global life
+    image.set_data(life)
+    life = game(life, bc='Periodic')
+    biodensity = np.mean(life)*100
+    ax.set_title("Life after {} interations. \n Enviroment is {:.2f}% full of life".format(i+1,biodensity))
 
-_init(16,16)
-
-fig, ax = plt.subplots()
-matrice = ax.matshow(M)
-plt.colorbar(matrice)
-
-ani = animation.FuncAnimation(fig, update, frames=19, interval=500)
+ani = animation.FuncAnimation(fig, update,repeat=False ,frames=50, interval=500)
 plt.show()
